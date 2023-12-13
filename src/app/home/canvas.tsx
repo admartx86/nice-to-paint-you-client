d543344368ee9c01f7940dfe8905845cd9667823
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-12-13 14:17:30 -0600

    feat: Add zoom in and zoom out with ctrl + and ctrl -

--- src/app/home/canvas.tsx
+++ src/app/home/canvas.tsx
@@ -3,6 +3,61 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 const Canvas = () => {
+
+  const [zoomLevel, setZoomLevel] = useState(1);
+  const drawingActions = useRef([]); // Store drawing actions
+  const minZoom = 0.5;
+  const maxZoom = 3;
+
+  const applyZoom = (newZoomLevel) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+    ctx.scale(newZoomLevel, newZoomLevel);
+
+    // Redraw the last saved state from history at the new scale
+    if (history.length > 0) {
+      const image = new Image();
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, canvas.width * newZoomLevel, canvas.height * newZoomLevel);
+      };
+      image.src = history[history.length - 1];
+    }
+
+    ctx.restore();
+  };
+
+  const handleZoom = (e) => {
+    if (e.ctrlKey) {
+      e.preventDefault(); // This prevents the browser's default zoom
+  
+      let newZoomLevel = zoomLevel;
+  
+      if ((e.key === '+' && e.type === 'keydown') || (e.type === 'wheel' && e.deltaY < 0)) {
+        // Zoom in
+        newZoomLevel = Math.min(zoomLevel * 1.1, maxZoom);
+      } else if ((e.key === '-' && e.type === 'keydown') || (e.type === 'wheel' && e.deltaY > 0)) {
+        // Zoom out
+        newZoomLevel = Math.max(zoomLevel / 1.1, minZoom);
+      }
+  
+      setZoomLevel(newZoomLevel);
+      applyZoom(newZoomLevel);
+    }
+  };
+  
+  useEffect(() => {
+    window.addEventListener('keydown', handleZoom);
+    window.addEventListener('wheel', handleZoom, { passive: false });
+  
+    return () => {
+      window.removeEventListener('keydown', handleZoom);
+      window.removeEventListener('wheel', handleZoom);
+    };
+  }, [zoomLevel]);
+  
+
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [history, setHistory] = useState<string[]>([]);
@@ -22,36 +77,66 @@ const Canvas = () => {
     }
   }, []);
 
-  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    const { offsetX, offsetY } = e.nativeEvent;
+  const startDrawing = (e) => {
+    e.preventDefault(); // Prevent default behavior for touch events
+  
+    let offsetX, offsetY;
+    if (e.touches) {
+      // Handle touch events
+      offsetX = e.touches[0].clientX - canvasRef.current.getBoundingClientRect().left;
+      offsetY = e.touches[0].clientY - canvasRef.current.getBoundingClientRect().top;
+    } else {
+      // Handle mouse events
+      offsetX = e.nativeEvent.offsetX;
+      offsetY = e.nativeEvent.offsetY;
+    }
+  
     setIsDrawing(true);
-
     const ctx = canvasRef.current.getContext('2d');
     if (ctx) {
       ctx.beginPath();
-      ctx.moveTo(offsetX, offsetY); // Start the path at the current mouse position
-      ctx.lineTo(offsetX, offsetY); // Draw a tiny line segment at the same point
-      ctx.stroke(); // Render the stroke, which will appear as a dot
+      ctx.moveTo(offsetX, offsetY);
+      ctx.lineTo(offsetX, offsetY);
+      ctx.stroke();
     }
   };
+  
+  // const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+  //   const { offsetX, offsetY } = e.nativeEvent;
+  //   setIsDrawing(true);
+
+  //   const ctx = canvasRef.current.getContext('2d');
+  //   if (ctx) {
+  //     ctx.beginPath();
+  //     ctx.moveTo(offsetX, offsetY); // Start the path at the current mouse position
+  //     ctx.lineTo(offsetX, offsetY); // Draw a tiny line segment at the same point
+  //     ctx.stroke(); // Render the stroke, which will appear as a dot
+  //   }
+  // };
 
   const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
     if (!isDrawing) return;
 
-    const ctx = canvasRef.current.getContext('2d');
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
     if (!ctx) return;
 
-    const { offsetX, offsetY } = e.nativeEvent;
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
 
-    ctx.lineWidth = 10; // Width of the line (also the diameter of the circle)
-    ctx.lineCap = 'round'; // Creates a rounded end of the line
-    ctx.strokeStyle = 'black'; // Set the stroke color
+    // Adjust mouse coordinates for zoom level
+    const x = (e.clientX - rect.left) * scaleX / zoomLevel;
+    const y = (e.clientY - rect.top) * scaleY / zoomLevel;
 
-    ctx.lineTo(offsetX, offsetY); // Draw a line to the current point
-    ctx.stroke(); // Apply the stroke
+    ctx.lineWidth = 10 / zoomLevel; // Adjust line width based on zoom level
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
 
-    ctx.beginPath(); // Begin a new path for the next segment
-    ctx.moveTo(offsetX, offsetY); // Move the path to the current point without creating a line
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
   };
 
   const stopDrawing = () => {
@@ -120,6 +205,7 @@ const Canvas = () => {
   }, []);
 
   return (
+    <div>
     <canvas
       ref={canvasRef}
       onMouseDown={startDrawing}
@@ -128,6 +214,10 @@ const Canvas = () => {
       onMouseMove={draw}
       style={{ border: '2px solid black', width: '100%', height: '500px' }}
     />
+    "zoomLevel"={zoomLevel}
+    Canvas width ={canvasRef.current?.width}
+    Canvas height ={canvasRef.current?.height}
+    </div>
   );
 };
 
