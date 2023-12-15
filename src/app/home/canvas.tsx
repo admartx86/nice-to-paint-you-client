3ffe2b9267d12df75146298848b9303898a252cc
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-12-15 13:57:30 -0600

    feat: Add panning with ctrl + p

--- src/app/home/canvas.tsx
+++ src/app/home/canvas.tsx
@@ -3,37 +3,133 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 const Canvas = () => {
+  const mouseIsDown = useRef(false);
+  const lastMousePosition = useRef({ x: 0, y: 0 });
 
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  const [history, setHistory] = useState<string[]>([]);
+  const [redoHistory, setRedoHistory] = useState<string[]>([]);
+  const drawingActions = useRef([]);
+
+  const [panOffset, setPanOffset] = useState({ x: 0, y: 0 });
+  const isPanning = useRef(false);
+  
   const [zoomLevel, setZoomLevel] = useState(1);
-  const drawingActions = useRef([]); // Store drawing actions
   const minZoom = 0.5;
   const maxZoom = 3;
 
-  const applyZoom = (newZoomLevel) => {
+  useEffect(() => {
+    redrawCanvas();
+  }, [panOffset]);
+
+  //Not so clear on this...
+  useEffect(() => {
+    const handleKeydown = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === 'z') {
+        undoLast();
+      } else if (e.ctrlKey && e.key === 'y') {
+        redoLast();
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, [history, redoHistory]);
+
+  //Why use useEffect for this?
+  useEffect(() => {
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.scale(newZoomLevel, newZoomLevel);
-
-    // Redraw the last saved state from history at the new scale
-    if (history.length > 0) {
-      const image = new Image();
-      image.onload = () => {
-        ctx.drawImage(image, 0, 0, canvas.width * newZoomLevel, canvas.height * newZoomLevel);
-      };
-      image.src = history[history.length - 1];
+    if (canvas) {
+      canvas.width = canvas.offsetWidth;
+      canvas.height = canvas.offsetHeight;
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 5;
+      }
     }
+  }, []);
 
-    ctx.restore();
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'p' || e.key === 'P') {
+        isPanning.current = true;
+      }
+    };
+
+    const handleKeyUp = (e) => {
+      if (e.key === 'p' || e.key === 'P') {
+        isPanning.current = false;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleZoom);
+    window.addEventListener('wheel', handleZoom, { passive: false });
+
+    return () => {
+      window.removeEventListener('keydown', handleZoom);
+      window.removeEventListener('wheel', handleZoom);
+    };
+  }, [zoomLevel]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      saveState();
+    }
+  }, []);
+
+  const onMouseDown = (e) => {
+    mouseIsDown.current = true;
+    if (isPanning.current) {
+      startPan(e);
+    } else {
+      startDrawing(e);
+    }
+  };
+
+  const onMouseUp = () => {
+    mouseIsDown.current = false;
+    stopDrawing();
+    stopPan();
+  };
+
+  const onMouseMove = (e) => {
+    if (mouseIsDown.current) {
+      if (isPanning.current) {
+        pan(e);
+      } else {
+        draw(e);
+      }
+    }
+  };
+
+  const startPan = (e) => {
+    isPanning.current = true;
+    lastMousePosition.current = {
+      x: e.clientX,
+      y: e.clientY
+    };
   };
 
   const handleZoom = (e) => {
-    if (e.ctrlKey) {
+    if (e.ctrlKey && (e.key === '+' || e.key === '-')) {
       e.preventDefault(); // This prevents the browser's default zoom
-  
+
       let newZoomLevel = zoomLevel;
-  
+
       if ((e.key === '+' && e.type === 'keydown') || (e.type === 'wheel' && e.deltaY < 0)) {
         // Zoom in
         newZoomLevel = Math.min(zoomLevel * 1.1, maxZoom);
@@ -41,27 +137,11 @@ const Canvas = () => {
         // Zoom out
         newZoomLevel = Math.max(zoomLevel / 1.1, minZoom);
       }
-  
+
       setZoomLevel(newZoomLevel);
       applyZoom(newZoomLevel);
     }
   };
-  
-  useEffect(() => {
-    window.addEventListener('keydown', handleZoom);
-    window.addEventListener('wheel', handleZoom, { passive: false });
-  
-    return () => {
-      window.removeEventListener('keydown', handleZoom);
-      window.removeEventListener('wheel', handleZoom);
-    };
-  }, [zoomLevel]);
-  
-
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [history, setHistory] = useState<string[]>([]);
-  const [redoHistory, setRedoHistory] = useState<string[]>([]);
 
   const saveState = () => {
     if (canvasRef.current) {
@@ -71,15 +151,9 @@ const Canvas = () => {
     }
   };
 
-  useEffect(() => {
-    if (canvasRef.current) {
-      saveState();
-    }
-  }, []);
-
   const startDrawing = (e) => {
     e.preventDefault(); // Prevent default behavior for touch events
-  
+
     let offsetX, offsetY;
     if (e.touches) {
       // Handle touch events
@@ -90,7 +164,7 @@ const Canvas = () => {
       offsetX = e.nativeEvent.offsetX;
       offsetY = e.nativeEvent.offsetY;
     }
-  
+
     setIsDrawing(true);
     const ctx = canvasRef.current.getContext('2d');
     if (ctx) {
@@ -100,25 +174,12 @@ const Canvas = () => {
       ctx.stroke();
     }
   };
-  
-  // const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
-  //   const { offsetX, offsetY } = e.nativeEvent;
-  //   setIsDrawing(true);
-
-  //   const ctx = canvasRef.current.getContext('2d');
-  //   if (ctx) {
-  //     ctx.beginPath();
-  //     ctx.moveTo(offsetX, offsetY); // Start the path at the current mouse position
-  //     ctx.lineTo(offsetX, offsetY); // Draw a tiny line segment at the same point
-  //     ctx.stroke(); // Render the stroke, which will appear as a dot
-  //   }
-  // };
 
   const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
     if (!isDrawing) return;
 
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas?.getContext('2d');
     if (!ctx) return;
 
     const rect = canvas.getBoundingClientRect();
@@ -126,8 +187,8 @@ const Canvas = () => {
     const scaleY = canvas.height / rect.height;
 
     // Adjust mouse coordinates for zoom level
-    const x = (e.clientX - rect.left) * scaleX / zoomLevel;
-    const y = (e.clientY - rect.top) * scaleY / zoomLevel;
+    const x = ((e.clientX - rect.left) * scaleX) / zoomLevel;
+    const y = ((e.clientY - rect.top) * scaleY) / zoomLevel;
 
     ctx.lineWidth = 10 / zoomLevel; // Adjust line width based on zoom level
     ctx.lineCap = 'round';
@@ -173,50 +234,102 @@ const Canvas = () => {
       };
       image.src = state;
     }
+    redrawCanvas();
   };
 
-  useEffect(() => {
-    const handleKeydown = (e: KeyboardEvent) => {
-      if (e.ctrlKey && e.key === 'z') {
-        undoLast();
-      } else if (e.ctrlKey && e.key === 'y') {
-        redoLast();
-      }
-    };
-
-    window.addEventListener('keydown', handleKeydown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeydown);
-    };
-  }, [history, redoHistory]);
-
-  useEffect(() => {
+  const redrawCanvas = () => {
+    
     const canvas = canvasRef.current;
-    if (canvas) {
-      canvas.width = canvas.offsetWidth;
-      canvas.height = canvas.offsetHeight;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 5;
-      }
+    const ctx = canvas.getContext('2d');
+ 
+    // Clear the visible part of the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.restore();
+    ctx.save();
+  
+    // Apply zoom level
+ 
+  
+    // Redraw the last saved state from history
+    if (history.length > 0) {
+      const image = new Image();
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0);
+      };
+      image.src = history[history.length - 1];
     }
-  }, []);
+ 
+    
+
+    ctx.scale(zoomLevel, zoomLevel);
+  
+    ctx.translate(panOffset.x, panOffset.y);
+
+    
+  };
+  
+  const pan = (e) => {
+    if (!isPanning.current) return;
+
+    const dx = e.clientX - lastMousePosition.current.x;
+    const dy = e.clientY - lastMousePosition.current.y;
+
+    setPanOffset((prevOffset) => ({
+      x: prevOffset.x + dx,
+      y: prevOffset.y + dy
+    }));
+
+    lastMousePosition.current = {
+      x: e.clientX,
+      y: e.clientY
+    };
+  };
+
+  const stopPan = () => {
+    isPanning.current = false;
+  };
+  
+  const applyZoom = (newZoomLevel) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.save();
+
+    ctx.scale(newZoomLevel, newZoomLevel);
+    ctx.translate(panOffset.x, panOffset.y); // Apply pan offset
+    
+
+    // Redraw the last saved state from history at the new scale
+    if (history.length > 0) {
+      const image = new Image();
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0, canvas.width * newZoomLevel, canvas.height * newZoomLevel);
+      };
+      image.src = history[history.length - 1];
+    } else {
+      redrawCanvas();
+    }
+
+    ctx.restore();
+  };
 
   return (
     <div>
-    <canvas
-      ref={canvasRef}
-      onMouseDown={startDrawing}
-      onMouseUp={stopDrawing}
-      onMouseOut={stopDrawing}
-      onMouseMove={draw}
-      style={{ border: '2px solid black', width: '100%', height: '500px' }}
-    />
-    "zoomLevel"={zoomLevel}
-    Canvas width ={canvasRef.current?.width}
-    Canvas height ={canvasRef.current?.height}
+      <canvas
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseOut={onMouseUp} // Using onMouseUp for consistency
+        onMouseMove={onMouseMove}
+        style={{ border: '2px solid black', width: '100%', height: '500px' }}
+      />
+      "zoomLevel"={zoomLevel}
+      Canvas width ={canvasRef.current?.width}
+      Canvas height ={canvasRef.current?.height}
+      isPanning {isPanning.current.toString()}
+      panOffset x={panOffset.x}
+      panOffset y={panOffset.y}
     </div>
   );
 };
