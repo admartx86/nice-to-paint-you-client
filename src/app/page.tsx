d1eb01c6541631eb870816f37cf83d2b6de6b81c
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-01-07 18:35:46 -0600

    fix: clear all existing canvas logic and replace with canvas using Fabric library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Canvas from './home/canvas';
 export default function Home() {
   return (
     <main>
-      <p className="text-6xl">👨‍🎨 "Nice to paint you!"</p>
-      <p className="text-6xl">👩‍🎨 "Nice to paint you, too!"</p>
       <Canvas />
     </main>
   );
